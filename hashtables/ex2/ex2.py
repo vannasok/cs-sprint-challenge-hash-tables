383f4103cc3da17b0e1eb377a5d89ff4574b2e4e
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-06-07 20:56:47 -0700

    ex1,2,3

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,23 @@ class Ticket:
         self.destination = destination
 
 
+cache = {}
+
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    route = []
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+
+    route.append(cache['NONE'])
+
+    for index in range(length):
+        if route[index] in cache:
+            if cache[route[index]] == route[0]:
+                continue
+            route.append(cache[route[index]])
 
     return route
